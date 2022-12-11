bba039ec24c9f46e4453149008f1c9d7dc40d693
qingzhixing <1418390991@qq.com>
2022-12-11 18:48:31 +0800

    读loader

--- src/boot.asm
+++ src/boot.asm
@@ -16,90 +16,92 @@ call print
 
 ; jmp error
 
+; 从硬盘中读loader
 mov edi,0x1000      ;读取的目标内存
-mov ecx,0           ;起始扇区
-mov bl,1            ;读取扇区数量 
+mov ecx,2           ;起始扇区
+mov bl,4            ;读取扇区数量 
 
 call read_disk
+
+cmp word [0x1000],0x55aa ;魔数，校验是否读取成功
+jnz error
+
+jmp 0:0x1002
 
 ; 阻塞
 jmp $
 
 read_disk:
-    ;设置读写扇区的数量
-    mov dx,0x1f2
-    mov al,bl
-    out dx,al
+
+    ; 设置读写扇区的数量
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
 
     inc dx; 0x1f3
-    mov al,cl; 起始扇区的前八位
-    out dx,al
+    mov al, cl; 起始扇区的前八位
+    out dx, al
 
     inc dx; 0x1f4
-    shr ecx,8
-    mov al,cl; 起始扇区的中八位
-    out dx,al
+    shr ecx, 8
+    mov al, cl; 起始扇区的中八位
+    out dx, al
 
     inc dx; 0x1f5
-    shr ecx,8
-    mov al,cl; 起始扇区的后八位
-    out dx,al
+    shr ecx, 8
+    mov al, cl; 起始扇区的高八位
+    out dx, al
 
     inc dx; 0x1f6
-    mov ecx,8
-    and cl,0b1111; 将高四位设置为0
+    shr ecx, 8
+    and cl, 0b1111; 将高四位置为 0
 
-    mov al,0b1110_0000
-    or al,cl
-    out dx,al; 主盘 - LBA模式 
+    mov al, 0b1110_0000;
+    or al, cl
+    out dx, al; 主盘 - LBA 模式
 
-    inc dx;0x1f7
-    mov al,0x20; 读硬盘
-    out dx,al
+    inc dx; 0x1f7
+    mov al, 0x20; 读硬盘
+    out dx, al
 
-    xor ecx,ecx; mov ecx,0
-    mov cl,bl; 得到读写扇区的数量
+    xor ecx, ecx; 将 ecx 清空
+    mov cl, bl; 得到读写扇区的数量
 
     .read:
-        push cx; 保存cx
+        push cx; 保存 cx
         call .waits; 等待数据准备完毕
         call .reads; 读取一个扇区
-        pop cx; 恢复cx
+        pop cx; 恢复 cx
         loop .read
+
     ret
 
     .waits:
-        mov dx,0x1f7
+        mov dx, 0x1f7
         .check:
-            in al,dx
+            in al, dx
             jmp $+2; nop 直接跳转到下一行
+            jmp $+2; 一点点延迟
             jmp $+2
-            jmp $+2; 一点延迟
-            and al,0b1000_1000
-            cmp al,0b0000_1000; 硬盘不繁忙且数据准备完毕
+            and al, 0b1000_1000
+            cmp al, 0b0000_1000
             jnz .check
         ret
 
     .reads:
-        mov dx,0x1f0
-        mov cx,256; 一个扇区 256 bits
+        mov dx, 0x1f0
+        mov cx, 256; 一个扇区 256 字
         .readw:
-            in ax,dx
+            in ax, dx
+            jmp $+2; 一点点延迟
             jmp $+2
             jmp $+2
-            jmp $+2; 延迟
-            mov [edi],ax
-            add edi,2; 往后继续读
-        loop .readw
+            mov [edi], ax
+            add edi, 2
+            loop .readw
         ret
 
 
-; 0xb8000 为文本显示器内存区域
-;mov ax,0xb800
-;mov ds,ax
-;mov byte [0], 'Q'
-;mov byte [2], 'Z'
-;mov byte [4], 'X'
 
 print:
     mov ah,0x0e
