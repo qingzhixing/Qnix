d17f3b7170bc58b517b8f295d275f4f571de6203
qingzhixing <1418390991@qq.com>
2022-12-17 21:07:59 +0800

    保护模式

--- src/loader.asm
+++ src/loader.asm
@@ -40,19 +40,7 @@ detect_memory:
     mov si,detecting
     call print
 
-    ; 结构体数量
-    mov cx, [ards_count]
-    ; 结构体指针
-    mov si, 0
-
-.show:
-    mov eax, [si+ards_buffer]
-    mov ebx, [si+ards_buffer+8]
-    mov edx, [si+ards_buffer+16]
-    add si,20
-    loop .show
-; 阻塞
-jmp $
+    jmp prepare_protected_mode
 
 print:
     mov ah,0x0e
@@ -81,6 +69,77 @@ error:
     hlt; 让cpu停止
     jmp $; 阻塞
     .msg db "Loading Qnix Error!!!", 10, 13, 0 
+
+prepare_protected_mode:
+    cli ; 关闭中断
+
+    ; 打开A20线
+    in al, 0x92
+    or al, 0b10
+    out 0x92,al
+
+    ; 加载gdt
+    lgdt [gdt_ptr]
+
+    ; 启动保护模式
+    mov eax,cr0
+    or eax,1
+    mov cr0,eax
+
+    ; 用跳转刷新缓存,启用保护模式
+    jmp dword code_selector:protected_mode
+
+[bits 32]
+protected_mode:
+    mov ax,data_selector
+    ; 初始化段寄存器
+    mov ds,ax
+    mov es,ax
+    mov fs,ax
+    mov gs,ax
+    mov ss,ax
+
+    mov esp,0x10000; 修改栈顶 0x7e00~0x9fbff任选一个地址 操作系统加载地址
+
+    mov byte [0xb8000], 'P'
+
+    mov byte [0x200000], 'P'
+
+jmp $; 阻塞
+
+code_selector equ (1 << 3)
+data_selector equ (2 << 3)
+
+memory_base equ 0; 内存开始的位置：基地址
+
+; 内存界限:4G/4k-1
+memory_limit equ ((1024*1024*1024*4) / (1024*4))-1
+
+gdt_ptr:
+    dw (gdt_end-gdt_base)-1
+    dd gdt_base
+gdt_base:
+    dd 0,0; NULL 描述符
+gdt_code:
+    dw memory_limit & 0xffff; 段界限的0-15位
+    dw memory_base & 0xffff; 基地址0-16位
+    db (memory_base >> 16) & 0xff; 基地址剩下八位
+    ; 存在 - dpl 0 - S _ 代码 - 非依从 - 可读 - 没有被访问过
+    db 0b_1_00_1_1_0_1_0 
+    ; 4k - 32位 - 不是64位 - 段界限 16~19
+    db 0b_1_1_0_0_0000 | (memory_limit>>16) & 0xf
+    db (memory_base >> 24) & 0xff
+gdt_data:
+    dw memory_limit & 0xffff; 段界限的0-15位
+    dw memory_base & 0xffff; 基地址0-16位
+    db (memory_base >> 16) & 0xff; 基地址剩下八位
+    ; 存在 - dpl 0 - S _ 数据 - 向上扩展 - 可写 - 没有被访问过
+    db 0b_1_00_1_0_0_1_0 
+    ; 4k - 32位 - 不是64位 - 段界限 16~19
+    db 0b_1_1_0_0_0000 | (memory_limit>>16) & 0xf
+    db (memory_base >> 24) & 0xff
+
+gdt_end:
 
 ards_count: 
     dw 0
