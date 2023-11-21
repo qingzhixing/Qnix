d3d6cc5bc4dfa3f549f233e6b30b65ffe63c2ba9
qingzhixing <qzsqqmail@qq.com>
2023-11-21 20:10:23 +0800

    放弃usb实体机启动

--- src/boot.asm
+++ src/boot.asm
@@ -134,10 +134,10 @@ ReadDisk:
         cmp dl,0b0000_0001
         pop dx
         
-        jne .Then
+        jne .Else
         call .ShowErrorMsg
 
-        .Then:
+        .Else:
 
         and al,0b1000_1000
         cmp al,0b0000_1000
@@ -158,9 +158,6 @@ ReadDisk:
         mov cx,256; 一个扇区256字(512Byte)
         .ReadDW:
             in ax,dx
-            jmp $+2
-            jmp $+2     ; 直接跳转到下一行，用于产生延迟
-            jmp $+2     ; 产生的延迟比nop多
             mov [edi],ax
             add edi,2
             loop .ReadDW
@@ -175,133 +172,22 @@ ReadDisk:
         call Print
         ret
 
-
-; 写入磁盘
-; 初始化操作:
-;   edi: 要写入硬盘的内存的起始地址
-;   ecx: 写入的起始扇区(LBA28模式)
-;   bl: 写入的扇区数
-WriteDisk:
-    ;0x1F2：读写扇区的数量
-    mov dx,0x1f2
-    mov al,bl
-    out dx,al
-
-    ;0x1F3：读写扇区起始地址0~7位
-    mov dx,0x1f3
-    mov al,cl
-    out dx,al
-
-    ;0x1F4：读写扇区起始地址8~15位
-    mov dx,0x1f4
-    shr ecx,8
-    mov al,cl
-    out dx,al
-
-    ;0x1F5：读写扇区起始地址16~23位
-    mov dx,0x1f5
-    shr ecx,8
-    mov al,cl
-    out dx,al
-
-    ;0x1F6：Device寄存器
-    ;   0 ~ 3：起始扇区的 24 ~ 27 位
-    ;   4: 0 主盘, 1 从片
-    ;   6: 0 CHS, 1 LBA
-    ;   5 ~ 7：固定为1
-    mov dx,0x1f6
-    shr ecx,8
-    ; 取最开始3bit
-    and cl,0b111;
-    mov al,cl
-    or al,0b1110_0000   ; 主盘 LBA模式
-    out dx,al
-
-    ;0x1F7：out时:Command寄存器
-    mov dx,0x1f7
-    mov al,0x30         ; 写硬盘
-    out dx,al
-
-    ; 清空
-    xor ecx,ecx
-    ; 得到读写扇区的数量
-    mov cl,bl
-
-    ; 读取硬盘
-
-    ; 等待数据准备完毕
-    ; 0x1F7: in时，作为Status寄存器
-    mov dx,0x1f7
-    .CheckStatus:
-        in al,dx
-        jmp $+2
-        jmp $+2     ; 直接跳转到下一行，用于产生延迟
-        jmp $+2     ; 产生的延迟比nop多
-
-        ; 判断是否产生错误
-        push dx
-        mov dx,ax
-        and dl,0b0000_0001
-        cmp dl,0b0000_0001
-        pop dx
-        
-        jne .Then
-        call .ShowErrorMsg
-
-        .Then:
-
-        and al,0b1000_0000
-        cmp al,0b0000_0000
-        ; 第七位为0，不繁忙则准备完毕:
-        ;    0 ERR
-        ;    3 DRQ 数据准备完毕
-        ;    7 BSY 硬盘繁忙
-        ; 否则继续等待
-        jne .CheckStatus
-
-    ; 循环写入
-    .WriteOneBlock:
-        ; 备份ecx
-        push ecx
-
-        ; 0x1F0: Data寄存器
-        mov dx,0x1f0
-        mov cx,256; 一个扇区256字(512Byte)
-        .WriteDW:
-            mov ax,[edi]
-            out dx,ax
-            jmp $+2
-            jmp $+2
-            jmp $+2    ; delay
-
-            add edi,2
-            loop .WriteDW
-        
-        ; 读取结束，恢复ecx
-        pop ecx
-        loop .WriteOneBlock
-
-    ret
-
-    .ShowErrorMsg:
-        mov si,diskErrorMsg
-        call Print
-        ret
-
 ErrorOccur:
     mov si,.msg
     call Print
     hlt ; CPU停止
     jmp $ ; 阻塞
     .msg:
-        db "Booting Error Occured!:(",10,13,0
+        db "Booting Error Occured! :(",10,13,0
 booting:
     ; 10,13,0 : \n \r \0
     db "Booting Qnix...",10,13,0
 readDiskMsg:
     db "Reading Disk...",10,13,0
 diskErrorMsg:
-    db "Disk Error Occured!",10,13,0
+    db "Disk Error Occured :(",10,13,0
+newLine:
+    db 10,13,0
 FillSector:
     ; 填充 0 到 510字节
     ; $ 当前行字节偏移
