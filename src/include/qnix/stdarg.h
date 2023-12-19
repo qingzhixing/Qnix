cf9ce7d0cbf8e0db02b1396228ea00d927635d78
qingzhixing <qzsqqmail@qq.com>
2023-12-19 20:19:01 +0800

    🍃 stdarg可变参数使用

--- src/include/qnix/stdarg.h
+++ src/include/qnix/stdarg.h
@@ -4,7 +4,10 @@
 
 typedef uint8_t* va_list;
 
+// 先将指针移动到参数所在内存再赋值
+// 加法是因为栈从高往低扩展，
 #define va_start(ap,v) (ap = (va_list)&v + sizeof(va_list))
+// 边移动边取值
 #define va_arg(ap,type) (*(type*)((ap += sizeof(va_list)) - sizeof(va_list)))
 #define va_end(ap) (ap = (va_list)0)
 #endif //QNIX_STDARG_H
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,11 +1,21 @@
 #include <qnix/qnix.h>
 #include <qnix/console.h>
 #include <qnix/string.h>
+#include <qnix/stdarg.h>
+
+void TestArgs(int cnt,...){
+    va_list args;
+    va_start(args,cnt);
+
+    int arg;
+    while(cnt--){
+        arg= va_arg(args,int);
+    }
+    va_end(args);
+}
 
 char message[] = "Hello Qnix\t Console!\n";
 void KernelInit() {
     ConsoleInit();
-    while(true){
-        ConsoleWrite(message,strlen(message));
-    }
+    TestArgs(5,1,0x55,0xaa,5,10);
 }
